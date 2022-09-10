6e105f04d0eb6ca75dcf2c74489d9981e019bdd4
Quinn-Quantum <79361383+Quinn-Quantum@users.noreply.github.com>
2022-09-10 12:43:28 +0200

    Player is moving

---
+++ Skripts/Player.gd
@@ -0,0 +1,39 @@
+extends KinematicBody
+
+
+const GRAVITY = 4
+const SPEED = 4
+const JUMP_POWER = 90
+const ROTATING = 4
+const UP_VECTOR = Vector2(0,-1)
+
+func _ready():
+	pass
+
+func _process(delta):
+	
+	move_and_collide(Vector3.DOWN * delta * GRAVITY)
+
+	
+	check_key_input(delta)
+
+
+func check_key_input(delta):
+	
+	if Input.is_action_pressed("left"):
+		rotate(Vector3.UP, ROTATING * delta)
+		move_and_collide(-transform.basis.x * SPEED * delta)
+	
+	if Input.is_action_pressed("right"):
+		rotate(Vector3.UP, -ROTATING * delta)
+		move_and_collide(transform.basis.x * SPEED * delta)
+	
+	if Input.is_action_pressed("forward"):
+		move_and_collide(-transform.basis.z * SPEED * delta)
+	
+	if Input.is_action_pressed("back"):
+		move_and_collide(transform.basis.z * SPEED * delta)
+		
+	if Input.is_action_just_pressed("jump"):
+		move_and_collide(transform.basis.y * JUMP_POWER * delta)
+	
